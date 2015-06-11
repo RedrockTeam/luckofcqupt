2be1b395159625797c1a24c049e52f4c305e93de
smelrain <840277005@qq.com>
2015-06-11 18:38:17 +0800

    fix

---
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,21 @@
+<?php if (!defined('THINK_PATH')) exit();?><!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <title>首页</title>
+    <script src="/luckofcqupt/Public/js/lib/init.js"></script>
+    <link rel="stylesheet" type="text/css" href="/luckofcqupt/Public/css/lib/base.css">
+    <link rel="stylesheet" type="text/css" href="/luckofcqupt/Public/css/index.css">
+</head>
+<body>
+<div class="g-container">
+    <i class="s-logo"></i>
+    <menu class="u-links">
+        <a href="/luckofcqupt/home/index/findSchoolfellow">寻找身边重邮人</a>
+        <a href="/luckofcqupt/home/index/information">完善或修改信息</a>
+    </menu>
+</div>
+</body>
+<script src="/luckofcqupt/Public/js/index.js"></script>
+</html>

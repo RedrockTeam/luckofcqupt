4a880d00324d85a6eee0bb1735260832fe176f35
Lich <longzy2013@gmail.com>
2015-06-11 22:46:39 +0800

    ...

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -88,13 +88,13 @@ class IndexController extends Controller {
             ->order("contact desc")
             ->limit($offset, 10)
             ->select();
-//        $count = count($sf);
-//        for ($i=0; $i<$count; $i++){
-//            $pos_fri = $this->getLocation($sf[$i]['openid']);
-//            if ($this->computeDis($pos_tar['lat'], $pos_fri['lat'], $pos_tar['long'], $pos_fri['long'])>1000){//lan->lat by Lich
-//                unset($sf[$i]);
-//            }
-//        }
+        $count = count($sf);
+        for ($i=0; $i<$count; $i++){
+            $pos_fri = $this->getLocation($sf[$i]['openid']);
+            if ($this->computeDis($pos_tar['lat'], $pos_fri['lat'], $pos_tar['long'], $pos_fri['long'])>1000){//lan->lat by Lich
+                unset($sf[$i]);
+            }
+        }
         foreach($sf as $v){
             $v['stuid'] = substr($v['stuid'], 0, 4).'级';
             $data[] = $v;

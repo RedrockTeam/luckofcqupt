5df4457962559087c0ba0ef35a7b93aecf1612b4
smelrain <840277005@qq.com>
2015-06-16 21:20:48 +0800

    添加需求

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -8,7 +8,7 @@ return array(
     "DB_PREFIX" => "knot_",
     "DB_TYPE" => "mysql",
     "DB_HOST" => "127.0.0.1",
-    "DB_NAME" => "cyKnot",
+    "DB_NAME" => "luckofcqupt",
     "DB_PORT" => 3306,
     "DB_USER" => "root",
     "DB_PWD" => "",

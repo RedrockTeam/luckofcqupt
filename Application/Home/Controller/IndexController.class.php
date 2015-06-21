1d7b4e724bb9af774e77b065459678e0d1e91c5b
Lich <longzy2013@gmail.com>
2015-06-21 15:02:47 +0800

    oauth2.0

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -10,15 +10,15 @@ class IndexController extends Controller {
      */
     //显示主页
     public function index(){
-//        $code = I('get.code');
-//        if($code == null){
-//            return redirect("https://open.weixin.qq.com/connect/oauth2/authorize?appid=$this->appid&redirect_uri=http%3a%2f%2fhongyan.cqupt.edu.cn%2fgame%2fpublic%2frealtakephotos&response_type=code&scope=snsapi_userinfo&state=sfasdfasdfefvee#wechat_redirect");
-//        }else{
-//            session('code', $code);
-//            $return =  json_decode($this->getOpenId());
-//            $openid = $return->data->openid;
-//        }
-        $openid = I('get.openid');
+        $code = I('get.code');
+        if($code == null){
+            return redirect("https://open.weixin.qq.com/connect/oauth2/authorize?appid=$this->appid&redirect_uri=http%3a%2f%2fhongyan.cqupt.edu.cn%2fgame%2fpublic%2frealtakephotos&response_type=code&scope=snsapi_userinfo&state=sfasdfasdfefvee#wechat_redirect");
+        }else{
+            session('code', $code);
+            $return =  json_decode($this->getOpenId());
+            $openid = $return->data->openid;
+        }
+//        $openid = I('get.openid');
         $info = $this->bindVerify($openid);
         $care = $this->getOpenidVerify($openid);
         if ($info->status != '200') {//绑定学号没
@@ -463,7 +463,7 @@ class IndexController extends Controller {
      *
      * */
     public function getHeadImgUrl($openid){
-//        $openid = 'ouRCyjpvLulo8TzHsMmGY2bTP13c';
+//        $openid = 'ouRCyjpvLulo8TzHsMmGY2bTP13c';杨奇凡的openid, 2333333
         $url = "http://Hongyan.cqupt.edu.cn/MagicLoop/index.php?s=/addon/Api/Api/userInfo";
         $timestamp = time();
         $string = "";
@@ -529,24 +529,40 @@ class IndexController extends Controller {
         return $rel;
     }
 
-//    private function getOpenId () {
-//        $code = Session('code');
-//        $time=time();
-//        $str = 'abcdefghijklnmopqrstwvuxyz1234567890ABCDEFGHIJKLNMOPQRSTWVUXYZ';
-//        $string='';
-//        for($i=0;$i<16;$i++){
-//            $num = mt_rand(0,61);
-//            $string .= $str[$num];
-//        }
-//        $secret =sha1(sha1($time).md5($string)."redrock");
-//        $t2 = array(
-//            'timestamp'=>$time,
-//            'string'=>$string,
-//            'secret'=>$secret,
-//            'token'=>$this->acess_token,
-//            'code' => $code,
-//        );
-//        $url = "http://hongyan.cqupt.edu.cn/MagicLoop/index.php?s=/addon/Api/Api/webOauth";
-//        return json_encode($this->curl_api($url, $t2));
-//    }
+    private function getOpenId () {
+        $code = session('code');
+        $time=time();
+        $str = 'abcdefghijklnmopqrstwvuxyz1234567890ABCDEFGHIJKLNMOPQRSTWVUXYZ';
+        $string='';
+        for($i=0;$i<16;$i++){
+            $num = mt_rand(0,61);
+            $string .= $str[$num];
+        }
+        $secret =sha1(sha1($time).md5($string)."redrock");
+        $t2 = array(
+            'timestamp'=>$time,
+            'string'=>$string,
+            'secret'=>$secret,
+            'token'=>$this->acess_token,
+            'code' => $code,
+        );
+        $url = "http://hongyan.cqupt.edu.cn/MagicLoop/index.php?s=/addon/Api/Api/webOauth";
+        return json_encode($this->curl_api($url, $t2));
+    }
+
+    /*curl通用函数*/
+        private function curl_api($url, $data=''){
+            // 初始化一个curl对象
+            $ch = curl_init();
+            curl_setopt ( $ch, CURLOPT_URL, $url );
+            curl_setopt ( $ch, CURLOPT_POST, 1 );
+            curl_setopt ( $ch, CURLOPT_HEADER, 0 );
+            curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
+            curl_setopt ( $ch, CURLOPT_POSTFIELDS, $data );
+            // 运行curl，获取网页。
+            $contents = json_decode(curl_exec($ch));
+            // 关闭请求
+            curl_close($ch);
+            return $contents;
+        }
 }
